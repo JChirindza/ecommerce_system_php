efb98ed95e569fa7ac161e3c0f9b422033db665a
jchirindza <johnchirindza@gmail.com>
2020-11-13 15:05:00 +0200

    Atualizacao de createUser.php
    Verificacao de existencia de email ao criar novo usuario.

--- php_action/createUser.php
+++ php_action/createUser.php
@@ -6,33 +6,32 @@ $valid['success'] = array('success' => false, 'messages' => array());
 
 if($_POST) {	
 
-	$userName 		= $_POST['userName'];
-	$uemail 		= $_POST['uemail'];
-	$upassword 		= md5($_POST['upassword']);
-	$userStatus 	= $_POST['userStatus'];
+	$userName 	= $_POST['userName'];
+	$uemail 	= $_POST['uemail'];
+	$upassword 	= md5($_POST['upassword']);
+	$permittion	= $_POST['permittion'];
+	$url 		= '../assests/images/photo_default.png';
+	
+	$sql1 = "SELECT * FROM users WHERE email = '$uemail' ";
+	$query1 = $connect->query($sql1);
+	$count = $query1->num_rows;
 
-	$type = explode('.', $_FILES['userImage']['name']);
-	$type = $type[count($type)-1];		
-	$url = '../assests/images/users/'.uniqid(rand()).'.'.$type;
-	if(in_array($type, array('gif', 'jpg', 'jpeg', 'png', 'JPG', 'GIF', 'JPEG', 'PNG'))) {
-		if(is_uploaded_file($_FILES['userImage']['tmp_name'])) {			
-			if(move_uploaded_file($_FILES['userImage']['tmp_name'], $url)) {
-				
-				$sql = "INSERT INTO users (username, email, password, user_image, active, status) 
-				VALUES ('$userName' , '$uemail', '$upassword', '$url', 1, '$userStatus')";
-				if($connect->query($sql) === TRUE) {
-					$valid['success'] = true;
-					$valid['messages'] = "Successfully Added";	
-				} else {
-					$valid['success'] = false;
-					$valid['messages'] = "Error while adding the members";
-				}
+	if ($count == 0) {
+		$sql = "INSERT INTO users (username, email, password, user_image, type, permittion, active, status) 
+		VALUES ('$userName' , '$uemail', '$upassword', '$url', 1, '$permittion', 1, 1)";
 
-			}	else {
-				return false;
-			}	// /else	
-		} // if
-	} // if in_array 		
+		if($connect->query($sql) === TRUE) {
+			$valid['success'] = true;
+			$valid['messages'] = "Successfully Added";	
+		} else {
+			$valid['success'] = false;
+			$valid['messages'] = "Error while adding the members";
+		}
+	}else{
+		$valid['success'] = false;
+		$valid['messages'] = "Existing email, please type another one!";
+	}
+	
 } // if in_array 		
 
 $connect->close();
