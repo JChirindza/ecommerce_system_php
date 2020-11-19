82e92cadb99b5eb52852091e210b337b896c433f
jchirindza <johnchirindza@gmail.com>
2020-11-19 16:55:15 +0200

    Redirecionamento SESSION's
    Implementacao de direcionamento por tipo de usuario;
    Clientes e Funcionario.

--- core.php
+++ core.php
@@ -6,7 +6,7 @@ require_once 'db_connect.php';
 
 // echo $_SESSION['userId'];
 
-if(!$_SESSION['userId']) {
+if($_SESSION['userType'] != 1) {
 	header('location: ../SistemaDeVendas_ControleDeStock/index.php');	
 } 
 
