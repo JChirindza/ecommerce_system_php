72c710be7aa1bb7a1716c8fe95da340bc4c9b232
João Chirindza <johnchirindza@gmail.com>
2021-01-03 03:18:25 +0200

    Create fetch_related.php
    permite visualizar produtos relacionados a um producto.

---
+++ ecommerce/php_action/fetch_related.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once 'db_connect.php';
+
+if(isset($_POST["product_id"])) {
+
+	$product_id = $_POST['product_id'];
+	
+	$sql1 = " SELECT * FROM product WHERE product_id = {$product_id} ";
+	$query1 = $connect->query($sql1);
+	$result1 = $query1->fetch_assoc();
+
+	$sql = " SELECT * FROM product WHERE active = '1' AND categories_id = {$result1['categories_id']} AND product_id != {$product_id} ORDER BY RAND() limit 8";
+	$result = $connect->query($sql);
+
+	$output = '';
+	if($result->num_rows > 0) { 
+		foreach($result as $row) {
+			$brandID = $row['brand_id'];
+
+			$sql2 = "SELECT brand_name FROM brands WHERE brand_id = '$brandID' ";
+			$query2 = $connect->query($sql2);
+			$result2 = $query2->fetch_assoc();
+
+			$output .= '
+			<div class="col-md-2">
+				<a href="product_details.php?product_id='. $row['product_id'] .'">
+					<div class="product-entry">
+						<div class="col-md-12 product-img">
+							<img src="../src/'. $row['product_image'] .'" class="img-fluid" style="height: 100px; " >
+						</div>
+						<div class="product-brand" style= "text-align: center;">Marca '. $result2['brand_name'] .' </div>
+						
+						<input type="hidden" name="product_id" id="product_id" value="'. $row['product_id'] .'" />
+					</div>
+				</a>
+			</div>
+			';
+		}
+	} else {
+		$output = '<h3>No Data Found</h3>';
+	}
+	
+	echo $output;
+}
+
+?>
+
