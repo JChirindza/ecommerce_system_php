06ed9a1d5e4ccf1013db120470a5e15befea3f9a
João Chirindza <johnchirindza@gmail.com>
2021-02-11 22:16:58 +0200

    Update navbar.php

--- ecommerce/includes/navbar.php
+++ ecommerce/includes/navbar.php
@@ -25,69 +25,75 @@ if(isset($_SESSION['userId'])) {
 
 <nav class="navbar navbar-expand-lg border-bottom shadow-sm bg-dark " >
 	<!-- Brand -->
-	<div class="col-md-3">
-		<a class="brand navbar-brand logo p-0 border" href="../index.php">ComputersOnly</a>
+	<div class="col-sm-2 col-md-4 col-lg-3">
+		<a class="navbar-brand logo p-0 border" href="../index.php">ComputersOnly</a>
 	</div>
-	<div class="input-group col-md-6 m-auto">
-		<input type="text" class="col-sm-10 rounded-left border-0" id="myInput" placeholder="Procurar..." name="procurar">
-		<div class="input-group-append">
-			<button type="button" class="btn btn-primary"><i class="fas fa-search fa-2x"></i></button>
-		</div>
-	</div>
-	<div class="col-md-3 row">	
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-			<ul class="navbar-nav">
-				<li class="nav-item">
-					<a class="nav-link text-white" href="home.php">Home</a>
-				</li>
-				<?php if(isset($_SESSION['userId'])){ ?>
-					<li class="nav-item">
-						<a href="cart.php" class="nav-item nav-link ">
-							<h6 class="px-5 cart text-white">
-								<i class="fas fa-cart-arrow-down fa-2x"></i>
-								<?php
-								if (isset($_SESSION['cartItem'])){
-									$count = count($_SESSION['cart']);
-									echo "<span id=\"cart_count\" class=\"badge badge-warning\">$count</span>";
-								}else{
-									echo "<span id=\"cart_count\" class=\"badge badge-secondary\">0</span>";
-								}
-								?>
-							</h6>
-						</a>
-					</li>
-				<?php } else { ?>
-					<li class="nav-item">
-						<a class="nav-link text-white" href="../sign-in.php">Login</a>
-					</li>
-				<?php } ?>
-				
-				
-			</ul>
-		</div>
-
-		<?php if(isset($_SESSION['userId'])){ ?>
-			<div class="dropdown navbar-nav float-right">
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav">
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<img class="img-profile rounded-circle border border-info" src="<?php echo $user_image_url; ?>"  style="width: 35px; height: 35px;">
-							</a>
-							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-								<div class="dropdown-header disabled text-center p-0 m-0 text-gray">Ola, <?php echo $username; ?></div>
-								<div class="dropdown-divider mt-0 pt-0"></div>
-								<a id="topNavSetting" class="dropdown-item" href="config.php"><i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>Configurações</a>
-								<div class="dropdown-divider"></div>
-								<a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal"><i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>Sair</a>
-							</div>
-						</li>
-					</ul>
-				</div>
+	<div class="col-sm-6 col-md-4 col-lg-6">
+		<div class="input-group col-12 m-auto">
+			<input type="text" class="col-10 rounded-left border-0" id="myInput" placeholder="Procurar..." name="procurar">
+			<div class="col-2 p-0 input-group-append">
+				<button type="button" class="btn btn-primary"><i class="fas fa-search"></i></button>
 			</div>
-		<?php } ?>
-		
+		</div>
 	</div>
+	<div class="col-sm-4 col-md-4 col-lg-3 d-flex justify-content-end">
+		<div class="row">
+			<div class="col-9 collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link text-white" href="home.php">Home</a>
+					</li>
+					<?php if(isset($_SESSION['userId'])){ ?>
+						<li class="nav-item">
+							<a href="cart.php" class="nav-item nav-link ">
+								<h6 class="px-5 cart text-white">
+									<i class="fas fa-cart-arrow-down fa-2x"></i>
+									<?php
+									if (isset($_SESSION['cartItem'])){
+										$count = count($_SESSION['cart']);
+										echo "<span id=\"cart_count\" class=\"badge badge-warning\">$count</span>";
+									}else{
+										echo "<span id=\"cart_count\" class=\"badge badge-secondary\">0</span>";
+									}
+									?>
+								</h6>
+							</a>
+						</li>
+					<?php } else { ?>
+						<li class="nav-item">
+							<a class="nav-link text-white" href="../sign-in.php">Login</a>
+						</li>
+					<?php } ?>
+				</ul>
+			</div>
+
+			<?php if(isset($_SESSION['userId'])){ ?>
+				<div class="col-3">
+					
+
+					<div class="dropdown navbar-nav float-right">
+						<div class="collapse navbar-collapse" id="navbarSupportedContent">
+							<ul class="navbar-nav">
+								<li class="nav-item dropdown">
+									<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+										<img class="img-profile rounded-circle border border-info" src="<?php echo $user_image_url; ?>"  style="width: 35px; height: 35px;">
+									</a>
+									<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+										<div class="dropdown-header disabled text-center p-0 m-0 text-gray">Ola, <?php echo $username; ?></div>
+										<div class="dropdown-divider mt-0 pt-0"></div>
+										<a id="topNavSetting" class="dropdown-item" href="config.php"><i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>Configurações</a>
+										<div class="dropdown-divider"></div>
+										<a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal"><i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>Sair</a>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			<?php } ?>
+		</div>
+	</div>
+	
 </nav>
 
 <!-- Logout Modal-->
