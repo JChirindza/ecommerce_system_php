2f76664b69499320e227e5bb40a2b432d7ab138c
João Chirindza <johnchirindza@gmail.com>
2021-06-15 18:33:41 +0200

    Update navbar.php
    atualizacao da URL ajax

--- includes/navbar.php
+++ includes/navbar.php
@@ -64,7 +64,7 @@ if (isset($_SESSION['userId'])) {
 	var userid = <?php echo $userID; ?>;
 	if(userid) {
 		$.ajax({
-			url: 'php_action/fetchSelectedUser.php',
+			url: 'php_action/ctrl_user.php?action=readSelected',
 			type: 'post',
 			data: {"userid": userid},
 			dataType: 'json',
