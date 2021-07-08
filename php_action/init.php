d3805a22b663d2b9a810aee6a256169bdc7975ba
João Chirindza <johnchirindza@gmail.com>
2021-07-09 00:39:42 +0200

    Update init.php
    Set Cookies on Language change.

--- php_action/init.php
+++ php_action/init.php
@@ -8,10 +8,14 @@ if (isset($_COOKIE['lang'])) {
 	$lang = Sys_Secure($_COOKIE['lang']);
 }
 if (isset($_GET['lang'])) {
+
 	$lang = Sys_Secure($_GET['lang']);
+
+	$cookie_name = "lang";
+	$cookie_value = $lang;
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
 }
 require_once 'includes/language/lang.' . $lang . '.php';
 // /Multi-lingual
-
 
 ?>
