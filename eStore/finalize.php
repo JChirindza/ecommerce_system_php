93b1892b18aecd7f3de8288e50d8aff2ea4d1f05
João Chirindza <johnchirindza@gmail.com>
2021-08-28 16:33:43 +0200

    Update finalizar.php
    Atualizacao da UI para Finalizar a compra.

--- eStore/finalize.php
+++ eStore/finalize.php
@@ -15,7 +15,7 @@ if( !(isset($_SESSION['userId']) && isset($_SESSION['userType'])) ) { ?>
 }
 ?>
 <?php 
-$cartId = $_GET['i'];
+$cartId = Sys_Secure($_SESSION['cartId']);
 // $cartId = 1;
 $sql = "SELECT * FROM cart WHERE cart_id = {$cartId}";
 $query = $connect->query($sql);
@@ -28,11 +28,11 @@ $result = $query->fetch_assoc();
 
 		<div class="m-0 p-0">
 			<ol class="breadcrumb bg-transparent m-0">
-				<li class="breadcrumb-item"><a href="home.php">Home</a></li>
-				<li class="breadcrumb-item"><a href="cart.php?c=cart">Cart</a></li>
-				<li class="breadcrumb-item"><a href="cart.php?c=cartItems&i=<?php echo $cartId; ?>">Cart items</a></li>
-				<li class="breadcrumb-item"><a href="checkout.php?i=<?php echo $_GET['i']; ?>">Checkout</a></li>
-				<li class="breadcrumb-item active">Finalize</li>
+				<li class="breadcrumb-item"><a href="home.php"><?php echo $language['home'] ?></a></li>
+				<li class="breadcrumb-item"><a href="cart.php?c=cart"><?php echo $language['cart'] ?></a></li>
+				<li class="breadcrumb-item"><a href="cart.php?c=cartItems"><?php echo $language['cart-items'] ?></a></li>
+				<li class="breadcrumb-item"><a href="checkout.php"><?php echo $language['checkout'] ?></a></li>
+				<li class="breadcrumb-item active"><?php echo $language['finalize'] ?></li>
 			</ol>
 		</div>
 
@@ -40,16 +40,20 @@ $result = $query->fetch_assoc();
 			<div class="col-md-12 col-md-offset-1">
 				<div class="process-wrap mt-4">
 					<div class="process text-center active">
-						<p><span><i class="fas fa-check"></i></span></p>
-						<label>Cart items</label>
+						<a href="cart.php?c=cartItems">
+							<p><span><i class="fas fa-check"></i></span></p>
+							<label><?php echo $language['cart-items'] ?></label>
+						</a>
 					</div>
 					<div class="process text-center active">
-						<p><span><i class="fas fa-check"></i></span></p>
-						<label>Checkout</label>
+						<a href="cart.php?checkout">
+							<p><span><i class="fas fa-check"></i></span></p>
+							<label><?php echo $language['checkout'] ?></label>
+						</a>
 					</div>
 					<div class="process text-center">
 						<p class="next"><span><i class="fas fa-check"></i></span></p>
-						<label>Finalize</label>
+						<label><?php echo $language['finalize'] ?></label>
 					</div>
 				</div>
 			</div>
@@ -58,65 +62,141 @@ $result = $query->fetch_assoc();
 		<div class="row mt-2 mt-md-4 mt-lg-4">
 			<div class="checkout col-sm p-4 bg-white">
 				<div class="d-sm-flex align-items-center justify-content-between">
-					<h4><i class="fas fa-list"></i> Checkout</h4>
+					<h4><i class="fas fa-list"></i> <?php echo $language['finalize'] ?></h4>
 				</div>
 				<hr>
 				<div class="row">
-					
-					<div class="cart-detail col-sm-12 col-md-7 col-lg-7 mb-3">
-						<h5>Selected items</h5>
-						<ul>
-							<?php 
-							$count = 0;
-							$total = 0;
+					<div class="col-sm-12 col-md-6 col-lg-6">
+						<h5 class=""><?php echo $language['delivery-location'] ?> <i class="fas fa-shipping-fast"></i></h5>
+						<!--  -->
+						<form class="form-horizontal" id="submitUserForm" action="php_action/ctrl_request.php?action=<?php //echo $ref; ?>" method="POST" enctype="multipart/form-data">
 
-							$sql = "SELECT * FROM cart_item WHERE cart_id = {$cartId}";
-							$resultado = mysqli_query($connect, $sql);
+							<div class="form-group  px-3 mt-4">
+								<div class="row">
+									<div class="col-sm-5">
+										<input type="text" class="form-control"  id="name" placeholder="<?php echo $language['name'] ?>" name="name" autocomplete="off" disabled>
+									</div>
 
-							while ($dados = mysqli_fetch_array($resultado)) { 
-
-								$sql2 = "SELECT * FROM product WHERE product_id = {$dados['product_id']} ";
-								$query = $connect->query($sql2);
-								$resultProduct = $query->fetch_assoc();
-
-								$total += $resultProduct['rate'] * $dados['quantity']; 
+									<div class="col-sm-5">
+										<input type="text" class="form-control" id="surname"  placeholder="<?php echo $language['surname'] ?>" name="surname" autocomplete="off" disabled>
+									</div>
+								</div>
+							</div> <!-- /form-group-->
+							<div class="form-group">
+								<label for="country" class="col-sm-6 control-label"><?php echo $language['country'] ?>: </label>
 								
-								echo '
-								<li>
-								<ul>
-								<li><span class="text-muted" data-toggle="tooltip" title="'.$resultProduct['product_name'].'">'.$dados['quantity'].' x '.$resultProduct['product_name'].'</span> <span data-toggle="tooltip" title="'.$dados['quantity'].' x '.number_format($resultProduct['rate'],2,",",".").'"> '.number_format($resultProduct['rate'] * $dados['quantity'],2,",",".").' Mt</span>
-								</li>
-								</ul>
-								</li>';
-							}
-							?>
-						</ul>
+								<div class="col-sm-10">
+									<select class="form-control" id="province" required>
+										<option>~~<?php echo $language['select'] ?>~~</option>
+										<option value="1"><?php echo $language['mozambique'] ?></option>
+									</select>
+								</div>
+							</div> <!-- /form-group-->
+
+							<div class="form-group">
+								<label for="province" class="col-sm-6 control-label"><?php echo $language['province'] ?>: </label>
+								
+								<div class="col-sm-10">
+									<select class="form-control" id="province" required>
+										<option>~~<?php echo $language['select'] ?>~~</option>
+										<option value="1">Maputo Cidade</option>
+										<option value="2">Maputo Provincia (Matola)</option>
+									</select>
+								</div>
+							</div> <!-- /form-group-->
+
+							<div class="form-group">
+								<label for="address" class="col-sm-6 control-label"><?php echo $language['address'] ?>: </label>
+								
+								<div class="col-sm-10">
+									<!-- <input type="text" class="form-control" id="address" placeholder="<?php echo $language['address'] ?>" name="address" autocomplete="off" required> -->
+									<textarea class="form-control" id="address" placeholder="<?php echo $language['address'] ?>" name="address" autocomplete="off" required></textarea>
+								</div>
+							</div> <!-- /form-group-->
+
+							<div class="form-group">
+								<label for="referencePoint" class="col-sm-6 control-label"><?php echo $language['reference-point'] ?>: </label>
+								
+								<div class="col-sm-10">
+									<input type="text" class="form-control" id="referencePoint" placeholder="<?php echo $language['reference-point'] ?>" name="referencePoint" autocomplete="off" required>
+								</div>
+							</div> <!-- /form-group-->
+
+							<div class="form-group">
+								<label for="postalCode" class="col-sm-6 control-label"><?php echo $language['postal-code'] ?>: </label>
+								
+								<div class="col-sm-10">
+									<input type="text" class="form-control" id="postalCode" placeholder="<?php echo $language['postal-code'] ?>" name="postalCode" autocomplete="off" required>
+								</div>
+							</div> <!-- /form-group-->
+							<button type="submit" class="btn btn-outline-success ml-3 rounded-0" id="createUserBtn" data-loading-text="Loading..." autocomplete="off"> <i class="fas fa-save"></i> <?php echo $language['save-changes'] ?></button>
+						</form>
 					</div>
 
-					<div class="col-sm-12 col-md-5 col-lg-5">
-						<h5 class="">Payment options <label class="badge badge-pill badge-success pl-4 pr-4">Finalize</label></h5>
-						<label class="text-muted"><i class="fas fa-info-circle pr-2"></i>Choose one of the following options to finalize:</label>
-						<div class="row pl-lg-1 d-flex justify-content-start">
-							<div class="btn btn-danger p-lg-3 m-lg-2 mpesa border rounded-lg">
-								<label class="font-weight-bold pl-4 pr-4"><i class="fas fa-mobile-alt fa-2x pr-md-2 pr-lg-2"></i>Mpesa</label>
+					<div class="cart-detail col-sm-12 col-md-6 col-lg-6 mb-3 mt-4 mt-md-0 mt-lg-0">
+						<h5 class=""><?php echo $language['order-resume'] ?></h5>
+						<div class="select-itens mt-4">
+							<h6>Cart items</h6>
+							<ul>
+								<?php 
+								$count = 0;
+								$total = 0;
+
+								$sql = "SELECT * FROM cart_item WHERE cart_id = {$cartId}";
+								$resultado = mysqli_query($connect, $sql);
+
+								while ($dados = mysqli_fetch_array($resultado)) { 
+
+									$sql2 = "SELECT * FROM product WHERE product_id = {$dados['product_id']} ";
+									$query = $connect->query($sql2);
+									$resultProduct = $query->fetch_assoc();
+
+									$total += $resultProduct['rate'] * $dados['quantity']; 
+
+									echo '
+									<li>
+									<ul>
+									<li><span class="text-muted" data-toggle="tooltip" title="'.$resultProduct['product_name'].'">'.$dados['quantity'].' x '.$resultProduct['product_name'].'</span> <span data-toggle="tooltip" title="'.$dados['quantity'].' x '.number_format($resultProduct['rate'],2,",",".").'"> '.number_format($resultProduct['rate'] * $dados['quantity'],2,",",".").' Mt</span>
+									</li>
+									</ul>
+									</li>';
+								}
+								?>
+							</ul>
+						</div>
+
+						<div class="pr-4 d-flex justify-content-end">
+							<span>
+								<strong class="text-muted"><?php echo $language['total'] ?>: </strong>
+							</span>
+							<span id="subTotal" class="font-weight-bold pl-2"> <?php echo number_format($total, 2,",","."); ?> Mt</span>
+						</div>
+						<hr>
+						<style type="text/css">
+							.payment-options img {
+								height: 75px;
+							}
+						</style>
+						<div class="payment-options mt-4 d-none d-sm-inline d-md-inline">
+							<h6><?php echo $language['payment-options'] ?></h6>
+							<div class="row">
+								<div class="mpesa col-12 col-lg-4 mb-2" title="M-pesa Max: 25.000,00 MTn"><img src="../assests/images/app/mpesa.png"></div>
+								<div class="visa col-12 col-sm-4 col-md-6 col-lg-4 mb-2" title="Visa"><img src="../assests/images/app/visa.png"></div>
+								<div class="mastercard col-12 col-sm-4 col-md-6 col-lg-4 mb-2" title="Mastercard"><img src="../assests/images/app/mastercard.png"></div>
 							</div>
-							<div class="btn btn-info p-lg-3 m-lg-2 paypal border rounded-lg" title="Paypal" >
-								<label class="font-weight-bold pl-4 pr-4"><i class="fab fa-paypal fa-2x pr-md-2 pr-lg-2"></i>Paypal</label>
-							</div>
+							<label class="text-muted mt-4"><i class="fas fa-info-circle"></i> M-pesa max: 25.000,00 MTn</label>
+
 						</div>
 					</div>
 				</div>
 				<hr>
-				<div class="d-flex justify-content-center">
-					<div class=" col-md-push-1 text-center">
-						<div class="grand-total border p-4">
-							<span>
-								<strong class="text-muted">Total: </strong>
-							</span> 
-							<span id="subTotal" class="font-weight-bold"><?php echo number_format($total, 2,",","."); ?> Mt</span>
-						</div>
-						<div class="">
-							<p><a href="cart.php?c=cartItems&i=<?php echo $cartId; ?>" class="btn btn-primary rounded-0"> <i class="fas fa-arrow-alt-circle-left mr-4"></i>Checkout </a></p>
+				<div class="row mx-1">
+					<div class="col-6">
+						<a href="checkout.php" class="btn btn-outline-secondary rounded-0"> <i class="fas fa-arrow-alt-circle-left mr-2"></i><?php echo $language['checkout'] ?> </a>
+					</div>
+					<div class="col-6">
+						<div class="d-inline">
+							<button class="btn btn-success rounded-0" data-toggle="modal" id="finalizePaymentModalBtn" data-target="#finalizePaymentModal"> <?php echo $language['finalize-and-pay'] ?> <i class="fas fa-arrow-alt-circle-right ml-2"></i></button>
 						</div>
 					</div>
 				</div>
@@ -124,6 +204,27 @@ $result = $query->fetch_assoc();
 		</div>
 	</div>
 </div>
+
+<!-- finalize payments -->
+<div class="modal fade" tabindex="-1" role="dialog" id="finalizePaymentModal">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title"><i class="fas fa-trash"></i> <?php echo $language['payment-options'] ?></h4>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			</div>
+			<div class="modal-body">
+				<div class="removeUserMessages"></div>
+				<p><?php echo $language['do-y-really-w-to-remove'] ?> ?</p>
+			</div>
+			<div class="modal-footer removeProductFooter">
+				<button type="button" class="btn btn-dark" data-dismiss="modal"> <i class="fas fa-times"></i></button>
+				<button type="button" class="btn btn-outline-danger" id="removeProductBtn" data-loading-text="Loading..."> <i class="fas fa-save"></i> <?php echo $language['remove'] ?></button>
+			</div>
+		</div><!-- /.modal-content -->
+	</div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
+<!-- /finalize payment -->
 
 <!-- <script src="custom/js/cart.js"></script> -->
 
