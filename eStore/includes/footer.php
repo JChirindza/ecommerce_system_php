72e1bf034e6a948f3c798c917c1005808aa15cde
João Chirindza <johnchirindza@gmail.com>
2021-09-03 21:05:25 +0200

    Footer.php Update
    Implementacao de um Link para trocar de idioma. Portugues ou Ingles.

--- eStore/includes/footer.php
+++ eStore/includes/footer.php
@@ -3,13 +3,27 @@
 		<a href="../sign-in.php" class="btn btn-warning btn-sm border border-dark pl-4 pr-4" data-toggle="tooltip" title="Sign-in for a better experience."><i class="fas fa-unlock"></i> <?php echo $language['sign-in'] ?></a>
 	</div>
 <?php } ?>
-<footer class="sticky-footer bg-white mt-1" style="margin-bottom: auto!important;">
-	<div class="container my-auto ">
-		<div class="copyright text-center my-auto">
-			<span>Copyright <i class="fas fa-copyright"></i> ComputersOnly | 2020 - <?php echo $anoAtual = date('Y'); ?></span>
-		</div>
+<div class="d-flex mt-4" id="wrapper">
+	<div class="container-fluid bg-white">
+		<footer class="sticky-footer px-2 py-4" style="margin-bottom: auto!important;" id="footer">
+			<div class="row">
+				<div class="col-6 copyright">
+					<span>Copyright <i class="fas fa-copyright"></i> ComputersOnly | 2020 - <?php echo $anoAtual = date('Y'); ?></span>
+				</div>
+
+				<div class="col-6 text-right d-flex justify-content-end">
+					<div class="pr-2"><i class="fas fa-globe text-secondary"></i></div>
+					<div class="language_link">
+						<a class="language_link_item fw-bolder" href="<?php echo $_SERVER['PHP_SELF']; ?>?lang=en" >English
+						</a> | 
+						<a class="language_link_item" href="<?php echo $_SERVER['PHP_SELF']; ?>?lang=pt" >Portugues
+						</a>
+					</div>
+				</div>
+			</div>
+		</footer>
 	</div>
-</footer>
+</div>
 </div> <!-- container -->
 
 <!-- ToolTip JS -->
