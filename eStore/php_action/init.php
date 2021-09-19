791d240bdd97ff9be6e1f9ad00e5717cca89c403
João Chirindza <johnchirindza@gmail.com>
2021-09-19 04:12:16 +0200

    Set conditions to lang cookie set and load.

--- eStore/php_action/init.php
+++ eStore/php_action/init.php
@@ -1,21 +1,43 @@
 <?php  
 // Multi-lingual
+
+// Valid Lang only => en/pt
+$sys['langs'] = array(
+    'en' => 'English',
+    'pt' => 'Português',
+);
+
 $lang = 'en';
 if (isset($_SESSION['lang'])) {
 	$lang = Sys_Secure($_SESSION['lang']);
 }
+
 if (isset($_COOKIE['lang'])) {
 	$lang = Sys_Secure($_COOKIE['lang']);
 }
+
+// Check if GET lang value is valid.
 if (isset($_GET['lang'])) {
 
-	$lang = Sys_Secure($_GET['lang']);
+	if (array_key_exists(strtolower($_GET['lang']), $sys['langs'])) {
+		$lang = Sys_Secure($_GET['lang']);
 
-	$cookie_name = "lang";
-	$cookie_value = $lang;
-    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+		$cookie_name = "lang";
+		$cookie_value = strtolower($lang);
+	    setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+	}
 }
-require_once '../includes/language/lang.' . $lang . '.php';
+
+$filename = '../includes/language/lang.'.$lang.'.php';
+
+// Check if concatenated path exists
+if (file_exists($filename)) {
+	$filename = '../includes/language/lang.'.$lang.'.php';
+}else{
+	// Default lang - english
+	$filename = '../includes/language/lang.en.php';
+}
+require_once "{$filename}";
 // /Multi-lingual
 
 ?>
