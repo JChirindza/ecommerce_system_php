26348cade8c991370bf5f86e244c51ad836592ce
João Chirindza <johnchirindza@gmail.com>
2021-10-03 14:17:53 +0200

    breadcrumb link update finalize.php

--- eStore/finalize.php
+++ eStore/finalize.php
@@ -132,7 +132,7 @@ $resultAddress = $query->fetch_assoc();
 			<ol class="breadcrumb bg-transparent m-0">
 				<li class="breadcrumb-item"><a href="home.php"><?php echo $language['home'] ?></a></li>
 				<li class="breadcrumb-item"><a href="cart.php?c=carts"><?php echo $language['carts'] ?></a></li>
-				<li class="breadcrumb-item"><a href="cart.php?c=cartItems"><?php echo $language['cart-items'] ?></a></li>
+				<li class="breadcrumb-item"><a href="cart.php?c=cartItems&i=<?php echo $cartId; ?>"><?php echo $language['cart-items'] ?></a></li>
 				<li class="breadcrumb-item"><a href="checkout.php"><?php echo $language['checkout'] ?></a></li>
 				<li class="breadcrumb-item active"><?php echo $language['finalize'] ?></li>
 			</ol>
