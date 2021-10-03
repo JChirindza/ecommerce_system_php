056655c8821ecb4788548096289f0a4c4568611e
João Chirindza <johnchirindza@gmail.com>
2021-10-03 14:19:37 +0200

    Remove redirection locations.php

--- eStore/locations.php
+++ eStore/locations.php
@@ -1,29 +1,4 @@
 <?php require_once 'includes/header.php'; ?>
-<?php  
-if( !(isset($_SESSION['userId']) && isset($_SESSION['userType'])) ) { ?>
-	<div class="p-4" style="height: 800px;">
-		<div class="alert alert-warning" role="alert">
-			<i class="fas fa-exclamation-triangle"></i>
-			<?php echo $language['access-403'] ?>.
-		</div>
-		<div class="d-flex justify-content-center">
-			<a href="../sign-in.php" class="btn btn-primary"><i class="fas fa-sign-in-alt pr-2"></i> <?php echo $language['sign-in'] ?></a>
-		</div>
-	</div>
-
-	<?php
-	require_once 'includes/footer.php';
-	die();
-}else{
-	$user_id = Sys_Secure($_SESSION['userId']);
-	$sql = "SELECT * FROM users WHERE user_id = {$user_id}";
-	$query = $connect->query($sql);
-	$result = $query->fetch_assoc();
-}
-
-$connect->close();
-?>
-
 <div class="d-flex" id="wrapper">
 	<div class="container-fluid bg-light ml-md-4 mr-md-4 ml-lg-4 mr-lg-4">
 
