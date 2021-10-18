2a8182c96dc0d1ab0eaa2995c234468c14312181
João Chirindza <johnchirindza@gmail.com>
2021-10-18 22:49:37 +0200

    Create script to confirm request.js

--- custom/js/request.js
+++ custom/js/request.js
@@ -2,7 +2,6 @@ var manageRequestTable;
 var requestedItemTable;
 
 $(document).ready(function() {
-
 	var divRequest = $(".div-request").text();
 
 	// nav bar 
@@ -24,3 +23,44 @@ $(document).ready(function() {
 		}
 	});
 });
+
+function confirm_request(cartHasPaidId = null) {
+
+	if(cartHasPaidId) {
+
+		// button loading
+		$("#confirmRequestBtn").button('loading');
+
+		$.ajax({
+			url: 'php_action/ctrl_request.php?action=confirm',
+			type: 'post',
+			data: {cartHasPaidId : cartHasPaidId},
+			dataType: 'json',
+			success:function(response) {
+				// button loading
+				$("#confirmRequestBtn").button('reset');
+
+				$("html, body, div.card, div.card-body").animate({scrollTop: '210px'}, 100);
+
+				if(response.success == true) {
+					$(".success-messages").html('<div class="alert alert-success p-2">'+
+					'<button type="button" class="close" data-dismiss="alert">&times;</button>'+
+					'<strong><i class="fas fa-save"></i></strong> '+ response.messages +
+					'<a type="button" onclick="printRequest('+response.cartHasPaidId+')" class="btn btn-primary btn-sm ml-4 mr-2"> <i class="fas fa-print"></i> Print </a>'+
+					'</div>');
+				} else {
+					$('.success-messages').html('<div class="alert alert-warning p-2">'+
+						'<button type="button" class="close" data-dismiss="alert">&times;</button>'+
+						'<strong><i class="fas fa-save"></i></strong> '+ response.messages +
+						'</div>');
+				} // /else
+
+				$(".alert-warning").delay(500).show(10, function() {
+					$(this).delay(3000).hide(10, function() {
+						$(this).remove();
+					});
+				}); // /.alert
+			} // /response messages
+		}); // /ajax function
+	}
+}
