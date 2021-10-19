aefbe3b5ae2a78b19e0eb84fe8f711f22b0a8ed3
João Chirindza <johnchirindza@gmail.com>
2021-10-19 14:26:00 +0200

    Update ctrl_request.php
    Create function print report, get request results and fetch.
    Add multi-lingual.

--- php_action/ctrl_request.php
+++ php_action/ctrl_request.php
@@ -1,5 +1,6 @@
 <?php
 require_once 'core.php';
+include 'init.php';
 
 if (isset($_GET['action']) && !empty($_GET['action'])) {
 	$action = Sys_Secure($_GET['action']);
@@ -13,6 +14,9 @@ if (isset($_GET['action']) && !empty($_GET['action'])) {
 		case 'confirm':
 		confirm_request();
 		break;
+		case 'printRequest':
+		printRequest();
+		break;
 		
 		// default:
 		// 	// code...
@@ -23,6 +27,9 @@ if (isset($_GET['action']) && !empty($_GET['action'])) {
 function fetchRequests(){
 	
 	global $connect;
+
+	global $language;
+
 
 	$output = array('data' => array());
 
@@ -41,8 +48,7 @@ function fetchRequests(){
 	INNER JOIN requests ON cart_has_paid.cart_has_paid_id = requests.cart_has_paid_id
 	INNER JOIN cart ON cart_has_paid.cart_id = cart.cart_id 
 	INNER JOIN clients ON cart.user_id = clients.user_id 
-	INNER JOIN users ON clients.user_id = users.user_id
-	ORDER BY cart_has_paid_id DESC";
+	INNER JOIN users ON clients.user_id = users.user_id";
 
 	$result = $connect->query($sql);
 
@@ -53,6 +59,7 @@ function fetchRequests(){
 		$x = 1;
 		while($row = $result->fetch_array()) {
 
+			$cartHasPaidId = $row['cart_has_paid_id'];
 			$dtResponded = $row['dt_requested'];
 			$clientName = $row['name']." ".$row['surname'];
 			$contact = $row['contact'];
@@ -60,9 +67,9 @@ function fetchRequests(){
 
  			// payment_status 
 			if($row['active'] == 1) {
-				$pendingStatus = "<label class='badge badge-warning'>Pending Request</label>";
+				$pendingStatus = "<label class='badge badge-warning'>".$language['pending-request']."</label>";
 			} else {
-				$pendingStatus = "<label class='badge badge-success'>Responded</label>";
+				$pendingStatus = "<label class='badge badge-success'>".$language['responded']."</label>";
 	 		} // /else
 
 	 		// payment_type 
@@ -74,8 +81,9 @@ function fetchRequests(){
 
 	 		$button = '<!-- Single button -->
 	 		<div class="btn-group">
-	 		<a href="request.php?r=respreq&i='.$row['cart_has_paid_id'].'" class="btn btn-outline-success btn-sm" id="cartItemBtn"> <i class="fas fa-eye"></i></a>
-	 		</div>';
+		 	<a href="request.php?r=respreq&i='.$cartHasPaidId.'" class="btn btn-outline-success btn-sm"> <i class="fas fa-eye"></i></a>
+		 	<button class="btn btn-outline-primary btn-sm" onclick="printRequest('.$cartHasPaidId.')"> <i class="fas fa-print"></i></button>
+		 	</div>';
 
 	 		$output['data'][] = array( 		
 	 			$x,
@@ -156,6 +164,7 @@ function fetchRequestedItems(){
 function confirm_request() {
 
 	global $connect;
+	global $language;
 
 	$valid['success'] = array('success' => false, 'messages' => array(), 'cart_has_paid_id' => '');
 	
@@ -176,19 +185,219 @@ function confirm_request() {
 					$valid['cart_has_paid_id'] = $cartHasPaidId;
 				}				
 			}else{
-				$valid['messages'] = "Error while confirm. This request was been confirmed!";
+				$valid['messages'] = "".$language['error-while-confirm-this-request-was-been-confirmed']."!";
 			}
 		}else{
-			$valid['messages'] = "Error while confirm request!";
+			$valid['messages'] = "".$language['error-while-confirm-request']."!";
 		}
 	}else {
-		$valid['messages'] = "Error while confirm request!";
+		$valid['messages'] = "".$language['error-while-confirm-request']."!";
 	}
 	$connect->close();
 
 	echo json_encode($valid);
 }
 
+function printRequest() {
+	global $connect;
+	global $language;
+
+	$cartHasPaidId = Sys_Secure($_POST['cartHasPaidId']);
+	
+	$sql = "SELECT cart_has_paid.dt_paid, 
+	users.name, 
+	users.surname, 
+	clients.contact,
+	cart_has_paid.sub_total,
+	cart_has_paid.vat,
+	cart_has_paid.total_amount,
+	cart_has_paid.discount,
+	cart_has_paid.grand_total,
+	cart_has_paid.payment_type,
+	(SELECT active 
+	FROM requests 
+	WHERE cart_has_paid_id = cart_has_paid.cart_has_paid_id) 
+	AS active
+	FROM cart_has_paid
+	INNER JOIN cart ON cart_has_paid.cart_id = cart.cart_id 
+	INNER JOIN clients ON cart.user_id = clients.user_id 
+	INNER JOIN users ON clients.user_id = users.user_id
+	WHERE cart_has_paid_id = {$cartHasPaidId} ORDER BY cart_has_paid_id DESC";
+	$query = $connect->query($sql);
+	$resultData = $query->fetch_array();
+
+	$dateRequested = $resultData['dt_paid'];
+	$clientName = $resultData['name']." ".$resultData['surname'];
+	$clientContact = $resultData['contact']; 
+	$subTotal = $resultData['sub_total'];
+	$vat = $resultData['vat'];
+	$totalAmount = $resultData['total_amount']; 
+	$discount = $resultData['discount'];
+	$grandTotal = $resultData['grand_total'];
+	$paymentType = $resultData['payment_type'];
+
+	$sql = "SELECT * FROM `cart_item_has_paid` WHERE cart_has_paid_id = {$cartHasPaidId}";
+	$itemResult = $connect->query($sql);
+
+	$table = '
+	<style>
+	label { border: 0; font: 14px Georgia, Serif; overflow: hidden; resize: none; }
+	table { border-collapse: collapse; }
+	table td, table th { border: 1px solid black; padding: 5px; }
+
+	   #header { height: 15px; width: 100%; margin: 20px 0; border: 1px solid black; background: #eee; text-align: center; color: black; font: bold 15px Helvetica, Sans-Serif; text-decoration: uppercase; letter-spacing: 20px; padding: 8px 0px; }
+
+	   #address { width: 250px; height: 150px; float: left; margin-left: .5rem}
+	   #customer { overflow: hidden; }
+
+	   #logo { text-align: right; float: right; position: relative; max-width: 540px; max-height: 100px; overflow: hidden; margin-right: .5rem;}
+	   #customer-title { font-size: 20px; font-weight: bold; float: left; }
+
+	   #client { margin-top: 1px; width: 300px; float: left; }
+	   #client td { text-align: right;  }
+	   #client td.meta-head { text-align: left; background: #eee; }
+	   #client td label { width: 100%; height: 20px; text-align: right; }
+
+	   #meta { margin-top: 1px; width: 300px; float: right; }
+	   #meta td { text-align: right;  }
+	   #meta td.meta-head { text-align: left; background: #eee; }
+	   #meta td label { width: 100%; height: 20px; text-align: right; }
+
+	.qty{ text-align: center; }
+
+	   #items { clear: both; width: 100%; margin: 30px 0 0 0; border: 1px solid black; }
+	   #items th { background: #eee; }
+	   #items label { width: 80px; height: 50px; }
+	   #items tr.item-row td { border:  1px solid black; vertical-align: top; }
+	   #items td.description { width: 300px; }
+	   #items td.item-name { width: 175px; }
+	   #items td.description label, #items td.item-name label { width: 100%; }
+	   #items td.total-line { border-right: 0; text-align: right; }
+	   #items td.total-value { border-left: 0; padding: 10px; font-weight:bold;}
+	   #items td.total-value label { height: 20px; background: none; }
+	   #items td.balance { background: #eee; }
+	   #items td.blank { border: 0; }
+
+	   #terms { text-align: center; margin: 20px 0 0 0; }
+	   #terms h5 { text-transform: uppercase; font: 13px Helvetica, Sans-Serif; letter-spacing: 10px; border-bottom: 1px solid black; padding: 0 0 8px 0; margin: 0 0 8px 0; }
+	   #terms label { width: 100%; text-align: center;}
+	</style>
+
+	<div id="page-wrap">
+
+	<p id="header">INVOICE</p>
+
+	<div id="identity">
+
+	<label id="address">
+	Computer Only Corp.;<br>
+	Av.: Karl Max Nr.: 1234;<br>
+	Maputo, Mocambique;<br>
+	Tel: (+258) 82 11 11 111; <br>
+	Email: computersonly@pconly.co.mz
+	</label>
+	<div id="logo">
+	<img id="image" src="assests/images/logo.png" alt="logo" />
+	</div>
+	</div>
+
+	<div style="clear:both"></div>
+
+	<div id="customer">
+
+	<table id="client">
+	<tr>
+	<td class="meta-head">'.$language['client-name'].': </td>
+	<td>'.$clientName.'</td>
+	</tr>
+	<tr>
+
+	<td class="meta-head">'.$language['contact'].': </td>
+	<td>'.$clientContact.'</td>
+	</tr>
+	<tr>
+	<td class="meta-head">NUIT: </td>
+	<td>3453453</div></td>
+	</tr>
+	</table>
+
+	<table id="meta">
+	<tr>
+	<td class="meta-head">Recibo & Data Nr.</td>
+	<td>'.$dateRequested.'</td>
+	</tr>
+	<tr>
+
+	<td class="meta-head">Date</td>
+	<td>'.$dateRequested.'</td>
+	</tr>
+	</table>
+	</div>
+
+	<table id="items">
+
+	<tr>
+	<th width="5%">#</th>
+	<th width="55%">'.$language['product-description'].'</th>
+	<th width="15%">'.$language['price'].'</th>
+	<th width="10%">'.$language['quantity'].'</th>
+	<th width="15%">'.$language['total-amount'].'</th>
+	</tr>
+	';
+	$x = 1;
+	while($row = $itemResult->fetch_array()) { 
+
+		$itemId = $row['cart_item_has_paid_id'];
+		$productId = $row['product_id'];
+		$quantity = $row['quantity'];
+		$paidPrice = $row['paid_price'];
+
+		$sql2 = "SELECT * FROM product WHERE product_id = {$productId}";
+		$query = $connect->query($sql2);
+		$resultProduct = $query->fetch_assoc();
+
+		$table .= '
+		<tr class="item-row"> 
+		<td class="item-name"><label>'.$x.'</label></td>
+		<td class="description"><label>'.$resultProduct['product_name'].'</label></td>
+		<td><label class="cost">'.number_format($paidPrice,2,",",".").'</label></td>
+		<td><label class="qty">'.$quantity.'</label></td>
+		<td><span class="price">'.number_format($paidPrice * $quantity,2,",",".").'</span></td>
+		</tr>
+		';
+		$x++;
+    } // /while
+    
+    $table.= '
+    <tr>
+    <td colspan="2" class="blank"></td>
+    <td colspan="2" class="total-line">'.$language['total-amount'].'</td>
+    <td class="total-value"><div id="subtotal">'.number_format($subTotal,2,",",".").'</div></td>
+    </tr>
+    <tr>
+    <td colspan="2" class="blank"></td>
+    <td colspan="2" class="total-line balance">Grande Total</td>
+    <td class="total-value balance"><div class="due">'.number_format($grandTotal,2,",",".").' MZN</div></td>
+    </tr>
+    </table>
+
+    <div class="">
+    <label>Assinatura do Funcionario:</label>
+    <br>
+    &nbsp;
+    </div>
+
+    <div id="terms">
+    <h5>Terms</h5>
+    <label>NET 30 Days. Finance Charge of 1.5% will be made on unpaid balances after 30 days.</label>
+    </div>
+
+    </div>
+    ';
+    $connect->close();
+
+    echo $table;
+}
 // Check if request was confirmed
 function is_request_confirmed($cartHasPaidId){
 	global $connect;
