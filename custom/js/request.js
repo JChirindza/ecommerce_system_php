d9af4db06892417ac05a3d9cd60f073ee517329e
João Chirindza <johnchirindza@gmail.com>
2021-10-19 14:23:43 +0200

    Request.js create scripts to print request report

--- custom/js/request.js
+++ custom/js/request.js
@@ -64,3 +64,32 @@ function confirm_request(cartHasPaidId = null) {
 		}); // /ajax function
 	}
 }
+
+// print request function
+function printRequest(cartHasPaidId = null) {
+
+	if(cartHasPaidId) {
+
+		$.ajax({
+			url: 'php_action/ctrl_request.php?action=printRequest',
+			type: 'post',
+			data: {cartHasPaidId: cartHasPaidId},
+			dataType: 'text',
+			success:function(response) {
+				var mywindow = window.open('', 'ComputersOnly - Sales & Management System', 'height=400,width=600');
+				mywindow.document.write('<html><head><title>Order Request Slip</title>');        
+				mywindow.document.write('</head><body>');
+				mywindow.document.write(response);
+				mywindow.document.write('</body></html>');
+
+		        mywindow.document.close(); // necessary for IE >= 10
+		        mywindow.focus(); // necessary for IE >= 10
+		        mywindow.resizeTo(screen.width, screen.height);
+		        setTimeout(function() {
+		        	mywindow.print();
+		        	mywindow.close();
+		        }, 1250);
+			}// /success function
+		}); // /ajax function to fetch the printable request
+	} // /else
+} // /print function
