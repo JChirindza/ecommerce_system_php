c697f1f621cafc19905dbb58f393ae3238a70b34
João Chirindza <johnchirindza@gmail.com>
2021-10-29 16:48:04 +0200

    Create about.php file

---
+++ eStore/about.php
@@ -0,0 +1,23 @@
+<?php require_once 'includes/header.php'; ?>
+<div class="d-flex" id="wrapper">
+	<div class="container-fluid bg-light ml-md-4 mr-md-4 ml-lg-4 mr-lg-4">
+
+		<div class="m-0 p-0">
+			<ol class="breadcrumb bg-transparent m-0">
+				<li class="breadcrumb-item"><a href="home.php"><?php echo $language['home'] ?></a></li>
+				<li class="breadcrumb-item active"><?php echo $language['about'] ?></li>
+			</ol>
+		</div>
+		
+		<div class="row mt-2 mt-md-0 mt-lg-0">
+			<div class="col-sm-12 bg-white p-3 userSettings">
+				<h4><i class="fas fa-map-marker-alt"></i> <?php echo $language['about'] ?></h4>
+				<div class="col-md-12 p-0 mt-2">
+					
+				</div> <!-- /col-md-12 -->
+			</div>
+		</div>
+	</div> <!-- / container-fluid -->
+</div> <!-- / wrapper -->
+
+<?php require_once 'includes/footer.php'; ?>
