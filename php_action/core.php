9621edb98b2febda900dac14fc8c7402c574b106
João Chirindza <johnchirindza@gmail.com>
2021-11-03 15:53:12 +0200

    update core.php
    redirect from management core.

--- php_action/core.php
+++ php_action/core.php
@@ -6,6 +6,6 @@ require_once 'ctrl_functions_general.php';
 require_once 'init.php';
 
 if(isset($_SESSION['userType']) && $_SESSION['userType'] != 1) {
-	header('location: index.php');	
+	header('location: eStore/home.php');	
 }
 ?>
