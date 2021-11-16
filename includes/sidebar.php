5a220867aeab7f0c97fe70712379840f5512c6f1
João Chirindza <johnchirindza@gmail.com>
2021-11-16 14:12:06 +0200

    Sidebar.php update UI | icons and text align

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,12 +3,22 @@ if( !(isset($_SESSION['userId']) && isset($_SESSION['userType'])) ) { ?>
 	<!-- Access deined - sidebar -->
 	<div class="border-right" id="sidebar-wrapper">
 		<div class="list-group list-group-flush">
-			<a id="navDashboard" href="dashboard.php" class="list-group-item list-group-item-action border-0"> <i class="fa fa-desktop fa-lg mr-2"></i> Dashboard</a>
-			<a id="navAbout" href="eStore/about.php" class="list-group-item list-group-item-action border-0"><i class="fas fa-cogs fa-lg mr-2"></i><?php echo $language['about']; ?></a>
+			<a id="navDashboard" href="dashboard.php" class="list-group-item list-group-item-action active"> 
+				<div class="row">
+					<div class="col-2"><i class="fa fa-desktop fa-lg"></i></div>
+					<div class="col-auto"><span class="item-name"><?php echo $language['dashboard']; ?></span></div>
+				</div>
+			</a>
+			<a id="navAbout" href="eStore/about.php" class="list-group-item list-group-item-action">
+				<div class="row">
+					<div class="col-2"><i class="fa fa-cogs fa-lg"></i></div>
+					<div class="col-auto"><span class="item-name"><?php echo $language['about']; ?></span></div>
+				</div>
+			</a>
 		</div>
 	</div>
 
-	<div class="col-10 p-4">
+	<div class="col-10 p-4 justify-content-center">
 		<!-- Alert message -->
 		<div class="alert alert-warning" role="alert">
 			<i class="fas fa-exclamation-triangle"></i>
@@ -16,7 +26,7 @@ if( !(isset($_SESSION['userId']) && isset($_SESSION['userType'])) ) { ?>
 		</div>
 		<!-- sign-in button -->
 		<div class="d-flex justify-content-center">
-			<a href="./sign-in.php" class="btn btn-primary"><i class="fas fa-sign-in-alt pr-2"></i> <?php echo $language['sign-in']; ?></a>
+			<a href="sign-in.php" class="btn btn-primary btn-sm border border-dark pl-4 pr-4" data-toggle="tooltip" title="<?php echo $language['sign-in-f-a-better-experience']; ?>"><i class="fas fa-unlock"></i> <?php echo $language['sign-in'] ?></a>
 		</div>
 	</div>	
 	<?php
@@ -26,16 +36,66 @@ if( !(isset($_SESSION['userId']) && isset($_SESSION['userType'])) ) { ?>
 	<!-- Sidebar -->
 	<div class="border-right" id="sidebar-wrapper">
 		<div class="list-group list-group-flush">
-			<a id="navDashboard" href="dashboard.php" class="list-group-item list-group-item-action border-0"> <i class="fa fa-desktop fa-lg mr-2 "></i> <?php echo $language['dashboard']; ?></a>
-			<a id="navOrder" href="orders.php?p=manord" class="list-group-item list-group-item-action border-0"><i class="fas fa-cart-arrow-down fa-lg mr-2"></i><?php echo $language['orders']; ?></a>
-			<a id="navRequest" href="request.php?r=manreq" class="list-group-item list-group-item-action border-0"><i class="fas fa-cart-plus fa-lg mr-2"></i><?php echo $language['requests']; ?></a>
-			<a id="navProduct" href="products.php?p=manprod" class="list-group-item list-group-item-action border-0"><i class="fab fa-product-hunt fa-lg mr-2"></i><?php echo $language['products']; ?></a>
-			<a id="navBrand" href="brands.php" class="list-group-item list-group-item-action border-0"><i class="fas fa-business-time fa-lg mr-2"></i><?php echo $language['brands']; ?></a>
-			<a id="navCategories" href="categories.php?c=manctg" class="list-group-item list-group-item-action border-0"><i class="fas fa-clipboard fa-lg mr-2"></i><?php echo $language['categories']; ?></a>
-			<a id="navUser" href="users.php" class="list-group-item list-group-item-action border-0"><i class="fas fa-users fa-lg mr-2"></i><?php echo $language['users'] ?></a>
-			<a id="navClient" href="clients.php" class="list-group-item list-group-item-action border-0"><i class="fas fa-people-arrows fa-lg mr-2"></i><?php echo $language['clients']; ?></a>
-			<a id="navReport" href="reports.php" class="list-group-item list-group-item-action border-0"><i class="fas fa-chart-line fa-lg mr-2"></i><?php echo $language['reports']; ?></a>
-			<a id="navSetting" href="profile.php" class="list-group-item list-group-item-action border-0"><i class="fas fa-user-cog fa-lg mr-2"></i><?php echo $language['profile']; ?></a>
+			<a id="navDashboard" href="dashboard.php" class="list-group-item list-group-item-action"> 
+				<div class="row">
+					<div class="col-2"><i class="fa fa-desktop fa-lg"></i></div>
+					<div class="col-auto"><span class="item-name"><?php echo $language['dashboard']; ?></span></div>
+				</div>
+			</a>
+			<a id="navOrder" href="orders.php?p=manord" class="list-group-item list-group-item-action">
+				<div class="row">
+					<div class="col-2"><i class="fas fa-cart-arrow-down fa-lg"></i></div>
+					<div class="col-auto"><span class="item-name"><?php echo $language['orders']; ?></span></div>
+				</div>
+			</a>
+			<a id="navRequest" href="request.php?r=manreq" class="list-group-item list-group-item-action">
+				<div class="row">
+					<div class="col-2"><i class="fas fa-cart-plus fa-lg"></i></div>
+					<div class="col-auto"><span class="item-name"><?php echo $language['requests']; ?></span></div>
+				</div>
+			</a>
+			<a id="navProduct" href="products.php?p=manprod" class="list-group-item list-group-item-action">
+				<div class="row">
+					<div class="col-2"><i class="fab fa-product-hunt fa-lg"></i></div>
+					<div class="col-auto"><span class="item-name"><?php echo $language['products']; ?></span></div>
+				</div>
+			</a>
+			<a id="navBrand" href="brands.php" class="list-group-item list-group-item-action">
+				<div class="row">
+					<div class="col-2"><i class="fas fa-business-time fa-lg"></i></div>
+					<div class="col-auto"><span class="item-name"><?php echo $language['brands']; ?></span></div>
+				</div>
+			</a>
+			<a id="navCategories" href="categories.php?c=manctg" class="list-group-item list-group-item-action">
+				<div class="row">
+					<div class="col-2"><i class="fas fa-clipboard fa-lg"></i></div>
+					<div class="col-auto"><span class="item-name"><?php echo $language['categories']; ?></span></div>
+				</div>
+			</a>
+			<a id="navUser" href="users.php" class="list-group-item list-group-item-action">
+				<div class="row">
+					<div class="col-2"><i class="fas fa-users fa-lg"></i></div>
+					<div class="col-auto"><span class="item-name"><?php echo $language['users']; ?></span></div>
+				</div>
+			</a>
+			<a id="navClient" href="clients.php" class="list-group-item list-group-item-action">
+				<div class="row">
+					<div class="col-2"><i class="fas fa-people-arrows fa-lg"></i></div>
+					<div class="col-auto"><span class="item-name"><?php echo $language['clients']; ?></span></div>
+				</div>
+			</a>
+			<a id="navReport" href="reports.php" class="list-group-item list-group-item-action">
+				<div class="row">
+					<div class="col-2"><i class="fas fa-chart-line fa-lg"></i></div>
+					<div class="col-auto"><span class="item-name"><?php echo $language['reports']; ?></span></div>
+				</div>
+			</a>
+			<a id="navSetting" href="profile.php" class="list-group-item list-group-item-action">
+				<div class="row">
+					<div class="col-2"><i class="fas fa-user-cog fa-lg mr-2"></i></div>
+					<div class="col-auto"><span class="item-name"><?php echo $language['profile']; ?></span></div>
+				</div>
+			</a>
 		</div>
 	</div>
 	<?php 
