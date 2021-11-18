3e2a6b5d29a9785d381348c7eed0f88c7330c268
João Chirindza <johnchirindza@gmail.com>
2021-11-18 18:39:05 +0200

    core.php update | add php function ( __DIR__ ) to get complete URL.

--- eStore/php_action/core.php
+++ eStore/php_action/core.php
@@ -1,9 +1,9 @@
 <?php 
 session_start();
 
-require_once '../php_action/db_connect.php';
-require_once '../php_action/ctrl_functions_general.php';
-require_once '../php_action/init.php';
+require_once __DIR__.'/../../php_action/db_connect.php';
+require_once __DIR__.'/../../php_action/ctrl_functions_general.php';
+require_once __DIR__.'/../../php_action/init.php';
 
 if(isset($_SESSION['userType']) && $_SESSION['userType'] != 2) {
 	header('location: ../dashboard.php');
